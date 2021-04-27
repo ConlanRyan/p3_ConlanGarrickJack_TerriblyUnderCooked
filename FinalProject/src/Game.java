031911a6af3c9ed53f9b4dae0c2ea6f930019750
peing <peing@Conlans-PC.attlocal.net>
2021-04-27 09:12:38 -0700

    Title Screen

--- FinalProject/src/Game.java
+++ FinalProject/src/Game.java
@@ -3,42 +3,55 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MouseInfo;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 public class Game extends JPanel implements ActionListener, KeyListener {
 	//800x600
 	//16x12 50 pixel tiles
 	private Player p = new Player(0,0);
     private Tile[][] room = new Tile[12][16];
+    private boolean title = true;
 	public void paint(Graphics g) {
 		super.paintComponent(g); // do not remove
-		p.paint(g);
-		
-		for(int i=0;i<room.length;i++) {
-			for(int j=0;j<room[0].length;j++) {
-				room[i][j].paint(g);
+		if (title) {
+			
+			g.drawImage(getImage("terriblyundercooked.png"),0,0,800,600,null);
+			g.setFont(new Font("courier",30,30));
+			g.setColor(new Color(255,255,255));
+			g.drawString("Press T to begin",33,400);
+		}
+		else {
+			p.paint(g);
+			
+			for(int i=0;i<room.length;i++) {
+				for(int j=0;j<room[0].length;j++) {
+					room[i][j].paint(g);
+				}
 			}
-		}
-		for(int i =0;i<800;i+=50) {
-			g.drawLine(i, 0, i, 600);
-		}
-		for(int i =0;i<600;i+=50) {
-			g.drawLine(0, i, 800, i);
+			for(int i =0;i<800;i+=50) {
+				g.drawLine(i, 0, i, 600);
+			}
+			for(int i =0;i<600;i+=50) {
+				g.drawLine(0, i, 800, i);
+			}
+			
+			int mouseY = ((int)MouseInfo.getPointerInfo().getLocation().getY())-35;
+			int mouseX = ((int)MouseInfo.getPointerInfo().getLocation().getX())-10;
+			
 		}
 		
-		
-		//test
-		int mouseY = ((int)MouseInfo.getPointerInfo().getLocation().getY())-35;
-		int mouseX = ((int)MouseInfo.getPointerInfo().getLocation().getX())-10;
 
 	}
 
@@ -55,6 +68,7 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 		frame.addKeyListener(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Timer t = new Timer(16, this);
+		
 		//LEVEL CREATION
 		for(int i =0;i<room.length;i++) {
 			for(int j=0;j<room[0].length;j++) {
@@ -67,6 +81,7 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 				
 			}
 		}
+		
 		t.start();
 		frame.getContentPane().setBackground(Color.black);
 		frame.setVisible(true);
@@ -107,6 +122,9 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 		if(e.getKeyCode()==69) {
 			
 		}
+		if(e.getKeyCode()==84) {
+			title=false;
+		}
 	}
 
 
@@ -144,7 +162,16 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
 		
 	}
-
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Game.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 
 
 } // end of class curly - do not delete

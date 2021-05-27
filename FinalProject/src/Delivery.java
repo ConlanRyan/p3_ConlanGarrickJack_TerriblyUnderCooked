614aa328cc8def6b3fe6e63c7d3943debd0ae731
plasm <plasm@LAPTOP-09R2RUI5>
2021-05-27 09:02:22 -0700

    Delivery Class

--- FinalProject/src/Delivery.java
+++ FinalProject/src/Delivery.java
@@ -1,4 +1,12 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.concurrent.TimeUnit;
 
-public class Delivery {
-
+public class Delivery extends Tile{
+	public Delivery(int x, int y, Player p) {
+		super(x, y, p);
+		imgName = "deliveryStation.png";
+		collide = true;
+	}	
 }
